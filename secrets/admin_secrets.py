cccd4b624542969e8be147f37e2c63e2f40b8997
dhillonsh <dhillonsh@gmail.com>
2016-10-30 12:26:24 -0700

    Update admin_secrets.py

--- secrets/admin_secrets.py
+++ secrets/admin_secrets.py
@@ -2,6 +2,6 @@
 # therefore be kept separate from the per-db client_secrets.py .
 #
 admin_user="cHk86MCN" 
-admin_pw="'j9VnWW3P2xqj8"       
+admin_pw="j9VnWW3P2xqj8"       
 port="27333"            
 host="localhost"  
