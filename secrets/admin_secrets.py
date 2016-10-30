a36f36b2a5a0ae6f660719a35d744d4146d1ddb8
dhillonsh <dhillonsh@gmail.com>
2016-10-30 12:27:38 -0700

    Update admin_secrets.py

--- secrets/admin_secrets.py
+++ secrets/admin_secrets.py
@@ -1,7 +1,7 @@
 # These secrets apply to the whole MongoDB instance and must
 # therefore be kept separate from the per-db client_secrets.py .
 #
-admin_user="cHk86MCN" 
-admin_pw="j9VnWW3P2xqj8"       
+admin_user="adminUser" 
+admin_pw="adminPass"       
 port="27333"            
 host="localhost"  
