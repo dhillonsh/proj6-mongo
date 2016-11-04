401df8151674c2524c80fb35a39cd39d2e9a671a
dhillonsh <dhillonsh@gmail.com>
2016-11-03 20:57:43 -0700

    Create extraFunctions.py

---
+++ extraFunctions.py
@@ -0,0 +1,47 @@
+from bson.objectid import ObjectId
+
+@app.template_filter( 'humanize' )
+def humanize_arrow_date( date ):
+    """
+    Date is internal UTC ISO format string.
+    Output should be "today", "yesterday", "in 5 days", etc.
+    Arrow will try to humanize down to the minute, so we
+    need to catch 'today' as a special case. 
+    """
+    try:
+        then = arrow.get(arrow.get(date).format('YYYY-MM-DD'))
+        now = arrow.get(arrow.utcnow().to('local').format('YYYY-MM-DD'))
+        if then.date() == now.date():
+            human = "Today"
+        else: 
+            human = then.humanize(now)
+            if human == "in a day":
+                human = "Tomorrow"
+    except: 
+        human = date
+    return human
+
+
+#############
+#
+# Functions available to the page code above
+#
+##############
+def get_memos():
+    """
+    Returns all memos in the database, in a form that
+    can be inserted directly in the 'session' object.
+    """
+    records = [ ]
+    for record in collection.find( { "type": "dated_memo" } ):
+        record['date'] = humanize_arrow_date(arrow.get(record['date']))
+        #del record['_id']
+        records.append(record)
+    return records 
+
+def add_memo(date, memo):
+    record = { "type": "dated_memo", "date":  arrow.get(date).naive.isoformat(), "text": memo }
+    collection.insert(record)
+
+def delete_memo(_id):
+    collection.remove({'_id': ObjectId(_id)})
