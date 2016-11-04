6c0b4894488e9861fac871fd4b3c337b169bc997
dhillonsh <dhillonsh@gmail.com>
2016-11-03 20:57:21 -0700

    Update flask_main.py

--- flask_main.py
+++ flask_main.py
@@ -19,6 +19,7 @@ from flask import request
 from flask import url_for
 
 from bson.objectid import ObjectId
+from extraFunctions import *
 
 import json
 import logging
@@ -103,53 +104,6 @@ def page_not_found(error):
 # Functions used within the templates
 #
 #################
-
-
-@app.template_filter( 'humanize' )
-def humanize_arrow_date( date ):
-    """
-    Date is internal UTC ISO format string.
-    Output should be "today", "yesterday", "in 5 days", etc.
-    Arrow will try to humanize down to the minute, so we
-    need to catch 'today' as a special case. 
-    """
-    try:
-        then = arrow.get(arrow.get(date).format('YYYY-MM-DD'))
-        now = arrow.get(arrow.utcnow().to('local').format('YYYY-MM-DD'))
-        if then.date() == now.date():
-            human = "Today"
-        else: 
-            human = then.humanize(now)
-            if human == "in a day":
-                human = "Tomorrow"
-    except: 
-        human = date
-    return human
-
-
-#############
-#
-# Functions available to the page code above
-#
-##############
-def get_memos():
-    """
-    Returns all memos in the database, in a form that
-    can be inserted directly in the 'session' object.
-    """
-    records = [ ]
-    for record in collection.find( { "type": "dated_memo" } ):
-        record['date'] = humanize_arrow_date(arrow.get(record['date']))
-        #del record['_id']
-        records.append(record)
-    return records 
-
-def add_memo(date, memo):
-    record = { "type": "dated_memo", "date":  arrow.get(date).naive.isoformat(), "text": memo }
-    collection.insert(record)
-
-def delete_memo(_id):
-    collection.remove({'_id': ObjectId(_id)})
       
 if __name__ == "__main__":
     app.debug=CONFIG.DEBUG
